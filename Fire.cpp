43be361f7e429d34249931868c1c951d774616c9
theragingscotdan <43002238+theragingscotdan@users.noreply.github.com>
2019-03-12 15:59:43 +0000

    comments added to fire.cpp

--- Fire.cpp
+++ Fire.cpp
@@ -12,14 +12,14 @@ Fire::Fire()
 
 void Fire::Update(sf::Time _frameTime)
 {
-	if (!m_canDamage)
+	if (!m_canDamage) // if the player cannot take damage
 	{
-		m_secondsPerStand += _frameTime.asSeconds();
+		m_secondsPerStand += _frameTime.asSeconds(); // start the timer for m_secondsPerStand
 		// If we have movement waiting to be processed,
-		if (m_secondsPerStand > 2.0f) // if the player cannot take damage and time is less than 2 seconds
+		if (m_secondsPerStand > 2.0f)  // If the time less than 2 seconds
 		{
 			m_canDamage = true; // allow the player to take damage
-			m_secondsPerStand = 0.0f;
+			m_secondsPerStand = 0.0f; // reset the timer back to 0
 
 		}
 	}
